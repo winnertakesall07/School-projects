765bd7f5091b789620879c570052866ae4650779
William Wind <tassen.puppen5r@icloud.com>
2025-10-14 09:30:20 +0200

    Add files via upload

---
+++ Mensa Array2.0/EssenListeMitWerten.java
@@ -0,0 +1,24 @@
+public class EssenListeMitWerten extends Listen {
+    public EssenListeMitWerten() {
+        super();
+        nameZahl = "Preis";
+        nameboolean = "Vegetarisch";
+        namen[0] = "Pizza";
+        booleanwert[0] = false; 
+        zahl[0] = 12;
+
+        namen[1] = "Salat";
+        booleanwert[1] = true; 
+        zahl[1] = 8;
+
+        namen[2] = "Burger";
+        booleanwert[2] = false;
+        zahl[2] = 10;
+
+        namen[3] = "Gemüsesuppe";
+        booleanwert[3] = true;
+        zahl[3] = 7;
+    }
+
+    
+}
