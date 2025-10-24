9088c5e848267599ddc4e7ae3025209527d6cb2b
William Wind <tassen.puppen5r@icloud.com>
2025-10-24 14:44:31 +0200

    Updated Projectile.java to add color system, pierce mechanic, explosion radius, and visual trail effects.

--- Shooter/Projectile.java
+++ Shooter/Projectile.java
@@ -5,7 +5,11 @@ import java.awt.Rectangle;
 public class Projectile extends Entity {
     int dx, dy;
     int damage;
-
+    private Color color = Color.YELLOW;
+    private int pierce = 0;
+    private int pierceCount = 0;
+    private int explosionRadius = 0;
+    
     public Projectile(int x, int y, int dx, int dy, int damage, int speed, GamePanel gp) {
         this.x = x;
         this.y = y;
@@ -14,6 +18,33 @@ public class Projectile extends Entity {
         this.damage = damage;
         this.speed = speed;
         this.solidArea = new Rectangle(0, 0, 8, 8);
+    }
+    
+    public void setColor(Color color) {
+        this.color = color;
+    }
+    
+    public void setPierce(int pierce) {
+        this.pierce = pierce;
+    }
+    
+    public void setExplosionRadius(int radius) {
+        this.explosionRadius = radius;
+    }
+    
+    public int getExplosionRadius() {
+        return explosionRadius;
+    }
+    
+    public boolean canPierce() {
+        return pierceCount < pierce;
+    }
+    
+    public void onHit() {
+        pierceCount++;
+        if (pierceCount >= pierce) {
+            alive = false;
+        }
     }
 
     @Override
@@ -29,11 +60,20 @@ public class Projectile extends Entity {
 
     @Override
     public void draw(Graphics2D g2) {
-        g2.setColor(Color.YELLOW);
+        // Draw trail effect
+        g2.setColor(new Color(color.getRed(), color.getGreen(), color.getBlue(), 100));
+        g2.fillRect(x - 6, y - 6, 12, 12);
+        
+        // Draw main projectile
+        g2.setColor(color);
         g2.fillRect(x - 4, y - 4, 8, 8);
     }
     
     public int getDamage() {
         return damage;
+    }
+    
+    public void setDamage(int damage) {
+        this.damage = damage;
     }
 }
