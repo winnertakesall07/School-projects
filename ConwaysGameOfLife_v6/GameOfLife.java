c10688b66c2829f772aecb24cd9c914ca35b3ab3
winnertakesall07 <tassen.puppen5r@icloud.com>
2026-05-07 08:33:58 +0200

    Delete ConwaysGameOfLife_v6 directory

--- ConwaysGameOfLife_v6/GameOfLife.java
+++
@@ -1,8 +0,0 @@
-public class GameOfLife {
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            GameOfLifeGUI gui = new GameOfLifeGUI();
-            gui.setVisible(true);
-        });
-    }
-}
